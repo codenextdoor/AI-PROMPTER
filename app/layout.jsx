42dc716f9d3672967f750b3726f3efe053be5146
codenextdoor <xuluv911@gmail.com>
2023-07-07 06:55:07 +0545

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/global.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html Lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
