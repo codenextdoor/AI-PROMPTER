f4678813d51e8ef5aaa5c5d51d6eb97a03a4d0d3
codenextdoor <xuluv911@gmail.com>
2023-07-08 20:15:18 +0545

    create post commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -13,7 +15,21 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
+      {/* Mobile navigation */}
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
