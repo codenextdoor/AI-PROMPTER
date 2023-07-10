29957b30b2914237e53e2b8f0240ebad92773c9f
codenextdoor <xuluv911@gmail.com>
2023-07-10 09:57:15 +0545

    routes commit

---
+++ app/api/auth/...nextauth/route.js
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [GoogleProvider({})],
+});
+
+// 01:19
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,7 @@ const Nav = () => {
   const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -80,8 +81,36 @@ const Nav = () => {
               height={37}
               className="rounded-full"
               alt="profile"
-              onClick={() => {}}
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
@@ -106,5 +135,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-// 1:12
