bcb05d4e716328e02e1e7975b86760e7c585e86d
codenextdoor <xuluv911@gmail.com>
2023-07-13 09:24:19 +0545

    mongodbsetup

--- app/api/auth/...nextauth/route.js
+++ app/api/auth/...nextauth/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+
+import User from "@models/user";
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -13,13 +16,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+
+      // check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      // if not create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace("", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
 export { handler as GET, handler as POST };
 
-// 01:19
+// 01:36
